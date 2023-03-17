fde04d8deb615bcc0ee662de4073cfc1ece38d20
Joakim Karlsson <j.w.karlsson@gmail.com>
2023-03-17 12:53:34 +0800

    Update create_image.sh

--- scripts/create_image.sh
+++ scripts/create_image.sh
@@ -15,7 +15,7 @@ set -o xtrace
 set -o verbose
 
 sudo docker build -t scope:debug -f Dockerfile .
-export SINGULARITY_CACHEDIR=/data/s_cache
-export SINGULARITY_TMPDIR=/data/s_tmp
-export SINGULARITY_LIBRARYDIR=/data/s_lib
+#export SINGULARITY_CACHEDIR=/data/s_cache
+#export SINGULARITY_TMPDIR=/data/s_tmp
+#export SINGULARITY_LIBRARYDIR=/data/s_lib
 singularity pull scope_debug.sif docker-daemon:scope:debug
